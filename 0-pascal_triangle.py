b6be3a7004c27a5df2dc7b77c37c9cd9affc2753
Rockjunior <r28081999m@gmail.com>
2024-10-04 06:24:40 -0700

    correction on Pascal_triangle file

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -3,22 +3,26 @@
 0-pascal_triangle
 """
 
-
 def pascal_triangle(n):
     """
     Returns a list of integers
-    representing the Pascal Triangle of n
-    returns empty list if n <= 0
+    representing the Pascal Triangle of n.
+    Returns an empty list if n <= 0.
     """
-    k = []
     if n <= 0:
-        return k
-    k = [[1]]
+        return []
+    
+    triangle = [[1]]  # Initialize the triangle with the first row
+
     for i in range(1, n):
-        temp = [1]
-        for j in range(len(k[i - 1]) - 1):
-            curr = k[i - 1]
-            temp.append(k[i - 1][j] + k[i - 1][j + 1])
-        temp.append(1)
-        k.append(temp)
-    return k
+        row = [1]  # Start each row with 1
+        last_row = triangle[i - 1]
+
+        # Generate the middle values for the current row
+        for j in range(len(last_row) - 1):
+            row.append(last_row[j] + last_row[j + 1])
+        
+        row.append(1)  # End each row with 1
+        triangle.append(row)  # Add the row to the triangle
+
+    return triangle
